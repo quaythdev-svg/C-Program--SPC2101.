03601e47e8f3244d0012ddedabf2319e0e7b04c8
quaythdev-svg <quaythdev@gmail.com>
2025-09-20 17:46:55 +0300

    Add files via upload

---
+++ Firstsemunits.c.c
@@ -0,0 +1,23 @@
+//simple c program (First Semester Units.)
+/*
+Name :Kimani Keth Gachunu.
+Registration Number:CT101/G/24892/24.
+Date:20/09/2025.
+Description:My First Semester Units.
+*/
+
+# include <stdio.h>//pre-processor directive printf()
+
+int main () {
+    printf("  FIRST SEMESTER UNITS   \n");
+    printf("1.Analogue Electroniics.\n");
+    printf("2.Discrete Mathematics.\n");
+    printf("3.Fundamentals of ICT.\n");
+    printf("4.Introduction to Computer Architecture.\n");
+    printf("5.Mathematics for Science.\n");
+    printf("6.Physics.\n");
+    printf("7.Introduction to Programming and Problem Solving.\n");
+    printf("8.Communication and information Literacy Skills.\n");
+    
+    return 0;
+}
