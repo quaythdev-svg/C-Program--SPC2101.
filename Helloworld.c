8a23869c6c6e2377ba8f9e0063ab32089d515b5e
quaythdev-svg <quaythdev@gmail.com>
2025-09-25 12:40:21 +0300

    Update and rename main.c to Helloworld.c

---
+++ Helloworld.c
@@ -0,0 +1,16 @@
+//Simple C Program (Hello World).
+/*
+Name:Kimani Keth Gachunu.
+Registration Number:CT101/24892/24.
+Date:12/09/2025.
+Description:First Attempt at Coding, Hello World.
+*/
+#include <stdio.h>// pre-processor directive printf().
+
+int main(){
+    
+    printf("Hello world!\n");
+    
+    return 0;
+}
+
