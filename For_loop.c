3f771727951a012ecb00690f599c3e1c673c53ae
quaythdev-svg <quaythdev@gmail.com>
2025-10-09 16:11:54 +0300

    Add files via upload

---
+++ For_loop.c
@@ -0,0 +1,17 @@
+//C Program (for loop)
+/*
+Name:Kimani Keth Gachunu.
+Registration Number:CT101/G/24892/24.
+Date:09/10/2025.
+Description:For Loop for decending order of numbers.
+*/
+
+#include <stdio.h> // Pre-processor Directive printf()
+
+int main() {
+    int i;
+	for( i=100; i >=50;i--){
+		printf("%d\n", i);
+	}
+	return 0;
+}
